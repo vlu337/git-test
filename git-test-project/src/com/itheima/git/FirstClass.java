30e021d577e19da4f9470688373b2a66ddc6d13b
vlu <386335886@qq.com>
2023-03-13 16:02:24 +0800

    添加git-test-project工程至git。

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello, git !");
+        System.out.println("hello, git !");
+        System.out.println("hello, git !");
+    }
+
+    public void test() {
+        int i = 0;
+        i += 10;
+        System.out.println(i);
+    }
+}
